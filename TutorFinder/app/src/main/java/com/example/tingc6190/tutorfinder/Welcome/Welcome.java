9f7d9c736dbe3c9479b1bb03dde0598bb5f4a765
Ting Chen <Tingc6190@gmail.com>
2018-11-02 11:46:49 -0400

    implemented location service to get the nearest zipcode of the device location

--- TutorFinder/app/src/main/java/com/example/tingc6190/tutorfinder/Welcome/Welcome.java
+++ TutorFinder/app/src/main/java/com/example/tingc6190/tutorfinder/Welcome/Welcome.java
@@ -1,9 +1,16 @@
 package com.example.tingc6190.tutorfinder.Welcome;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,19 +24,22 @@ import com.example.tingc6190.tutorfinder.MainActivity;
 import com.example.tingc6190.tutorfinder.R;
 import com.example.tingc6190.tutorfinder.Search.Search;
 
-public class Welcome extends Fragment {
+import static android.content.Context.LOCATION_SERVICE;
 
+public class Welcome extends Fragment implements LocationListener {
+
+    private static final int REQUEST_LOCATION_PERMISSIONS = 0x03001;
     private WelcomeListener welcomeListener;
     private String subject;
     private String zipcode;
+    private LocationManager locationManager;
+    public Double latitude;
+    public Double longitude;
+
 
     public Welcome() {
     }
 
-    public interface WelcomeListener
-    {
-        void getSearchSettings(String subject, String zipcode);
-    }
 
     @Override
     public void onAttach(Context context) {
@@ -49,6 +59,9 @@ public class Welcome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
+        requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_LOCATION_PERMISSIONS);
+
         return inflater.inflate(R.layout.content_welcome_screen, container, false);
     }
 
@@ -60,6 +73,7 @@ public class Welcome extends Fragment {
         {
             Button searchButton = getView().findViewById(R.id.search_welcome);
             Spinner subjectSpinner = getView().findViewById(R.id.setting_subject_spinner);
+
 
 
             ArrayAdapter<CharSequence> subjectAdapter = ArrayAdapter.createFromResource(getContext(),
@@ -88,7 +102,8 @@ public class Welcome extends Fragment {
 
                     Log.d("__SEARCH_TAPPED__", subject);
 
-                    welcomeListener.getSearchSettings(subject, "");
+                    //welcomeListener.getSearchSettings(subject, "");
+                    getLocation();
 
 //                    getFragmentManager().beginTransaction()
 //                            .replace(R.id.content_container, new Search())
@@ -96,6 +111,75 @@ public class Welcome extends Fragment {
                 }
             });
 
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //if permission not request, ask again
+        if (getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSIONS);
+        }
+
+        //show our map fragment
+        else
+        {
+
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public interface WelcomeListener
+    {
+        void getSearchSettings(String subject, String zipcode);
+    }
+
+    private void getLocation()
+    {
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+        {
+            // Request location updates using 'this' as our LocationListener.
+            //locationManager = new LocationManager();
+
+            locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+
+            //locationManager.requestLocationUpdates(Loca);
+
+            assert locationManager != null;
+            Location lastKnown = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (lastKnown != null)
+            {
+                latitude = lastKnown.getLatitude();
+                longitude = lastKnown.getLongitude();
+            }
+
+            Log.d("__LATITUDE___", String.valueOf(latitude));
+            Log.d("__LONGITUDE__", String.valueOf(longitude));
         }
     }
 }
